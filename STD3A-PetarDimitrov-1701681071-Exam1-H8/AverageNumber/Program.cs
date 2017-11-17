d0e845ea52b4c8781e2acb9ad717e046cec96237
PetarDimitrov <petio5678@abv.bg>
2017-11-17 09:56:06 +0200

    Initial commit.

---
+++ STD3A-PetarDimitrov-1701681071-Exam1-H8/AverageNumber/Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AverageNumber
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            List<int> evenNumbers = new List<int>();
+            int result = 1;
+
+            do
+            {
+                Console.WriteLine("Enter number:");
+                int.TryParse(Console.ReadLine(), out result);
+                if (result >= 1 && result <= 255)
+                {
+                    if (result % 2 == 0) evenNumbers.Add(result);
+                }
+
+            } while (result != 0);
+
+            if (evenNumbers.Count == 0)
+            {
+                Console.WriteLine("There aren't even numbers in this diapason [1, 255]");
+            }
+            else
+            {
+                Console.WriteLine("Finished !!!");
+                Console.WriteLine(new string('*', 20));
+                Console.WriteLine("\n");
+                int total = 0;
+                foreach (int item in evenNumbers)
+                {
+                    total += item;
+                }
+                Console.WriteLine("Avarage {0}", total / evenNumbers.Count);
+            }
+        }
+    }
+}
---
+++ STD3A-PetarDimitrov-1701681071-Exam1-H8/CircleArea/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CircleArea
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Random random = new Random();
+            int radius = random.Next(33, 187);
+            Console.WriteLine($"The circle random radius is = {radius}");
+            Console.WriteLine($"The circle area is = {(double)(Math.PI * radius * radius):f2}");
+        }
+    }
+}
